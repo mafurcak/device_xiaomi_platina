4b03fc65c413863eae49ce35449a603a02f415c9
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    platina: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ThermalController
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
