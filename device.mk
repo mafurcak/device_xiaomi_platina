aa5f768c1a104f604a565ef8c108b265857f4d20
Bruno Rocha <brunoxthe2@hotmail.com>
2019-09-16 08:15:34 -0300

    platina: manifest: drop default cam provider

--- device.mk
+++ device.mk
@@ -168,7 +168,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl:32 \
-    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libMiWatermark_shim \
     libxml2
